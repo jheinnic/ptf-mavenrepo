b82b7358e61cd8678eaf3419dbd388b6c94fc7b7
John Heinnickel` <jheinnic@gmail.com>
2019-03-04 09:44:33 -0800

    Pruned accidentally checked in maven target output files.

--- archetypes/command-side/target/test-classes/projects/basic/project/basic/event-model/src/main/lombok/it/pkg/basic/event/ExampleEvent.java
+++
@@ -1,9 +0,0 @@
-package name.jchein.portfolio.services.basic.event;
-
-import io.eventuate.Event;
-import io.eventuate.EventEntity;
-
-@EventEntity(entity="name.jchein.portfolio.services.basic.domain.Example")
-public interface ExampleEvent extends Event {
-	
-}
--- archetypes/query-side/target/test-classes/projects/basic/project/basic/service/src/main/java/it/pkg/basic/domain/ExampleCommand.java
+++
@@ -1,7 +0,0 @@
-package name.jchein.portfolio.services.basic.domain;
-
-import io.eventuate.Command;
-
-@SuppressWarnings("all")
-public interface ExampleCommand extends Command {
-}
